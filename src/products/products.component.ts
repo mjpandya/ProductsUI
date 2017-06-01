988db4edaaefcd2b8f4585e33d4f1eb62b8187ca
mjpandya <mihir.j.pandya@gmail.com>
2017-06-01 10:49:16 +0430

    Initial Commit

---
+++ src/products/products.component.ts
@@ -0,0 +1,23 @@
+/**
+ * Created by Pratham on 6/1/2017.
+ */
+import {Component} from '@angular/core';
+import {ProductsService} from './products.service';
+import {Http} from '@angular/http';
+
+@Component({
+  selector: 'phone-details',
+  templateUrl= 'product-component.html',
+  providers: [ProductsService]
+});
+
+export class SmartPhoneComponenet{
+  BrandName: string;
+  Model: string;
+  Price: string;
+  constructor(private http: Http){}
+
+  getSmartPhoneDetails(){
+   this.http.get('http://localhost:2550/getSmartPhoneDetails').subscribe();
+  }
+}
